44eae9e89f71ae298f3c26a02172a4bbf2d402f1
Mia Pritchina <deco-bots@yandex.ru>
2024-07-04 13:47:41 -0700

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js', // Adjust if the entry point is different
+    output: {
+        filename: 'fretboard.min.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'Fretboard',
+        libraryTarget: 'umd',
+    },
+    mode: 'production',
+};
