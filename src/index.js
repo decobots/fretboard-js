b1a6fd13c88b8f213d5bad3a7db3ebac141280a6
Mia Pritchina <deco-bots@yandex.ru>
2024-07-04 14:42:58 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ module.exports = {
   slide: require('./slide'),
   stepAndSlide: require('./stepAndSlide'),
 
-  getPitchSet: require('./chord').getPitchSet,
-  getPitchClassSet: require('./chord').getPitchClassSet,
-  getChord: require('./chord').getChord,
-  compareToChord: require('./chord').compareToChord,
+  getPitchSet: require('./chord'),
+
 }
